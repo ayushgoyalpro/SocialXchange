e9d910b312aea738d86a8113867758dc0c533066
Ayush Goyal <ayushgoyalpro@gmail.com>
2024-07-30 16:01:00 +0100

    First commit for backend

---
+++ soco-backend/src/main/java/com/socialxchange/soco_backend/SoCoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.socialxchange.soco_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoCoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoCoBackendApplication.class, args);
+	}
+
+}
---
+++ soco-backend/src/test/java/com/socialxchange/soco_backend/SoCoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialxchange.soco_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoCoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
